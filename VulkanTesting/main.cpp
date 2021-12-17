b359145588bb351323ac78f372133a2259662e92
Jingtao Huang <jingtaoh@usc.edu>
2021-12-16 21:37:57 -0800

    Initial Commit

---
+++ VulkanTesting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  VulkanTesting
+//
+//  Created by Lewis on 12/16/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
