835d4c2568de1a34df5fff6c997b2c3ea2595b13
Jingtao Huang <jingtaoh@usc.edu>
2021-12-29 22:48:08 -0800

    Change SPIR-V location

--- VulkanTesting/shaders/compile.sh
+++ VulkanTesting/shaders/compile.sh
@@ -1,7 +1,7 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    /home/jingtaoh/Codes/VulkanSDK/1.2.198.1/x86_64/bin/glslc shader.vert -o vert.spv
-    /home/jingtaoh/Codes/VulkanSDK/1.2.198.1/x86_64/bin/glslc shader.frag -o frag.spv
+    /home/jingtaoh/Codes/VulkanSDK/1.2.198.1/x86_64/bin/glslc shader.vert -o ../vert.spv
+    /home/jingtaoh/Codes/VulkanSDK/1.2.198.1/x86_64/bin/glslc shader.frag -o ../frag.spv
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    /Users/Lewis/Codes/VulkanSDK/1.2.198.1/macOS/bin/glslc shader.vert -o vert.spv
-    /Users/Lewis/Codes/VulkanSDK/1.2.198.1/macOS/bin/glslc shader.frag -o frag.spv
+    /Users/Lewis/Codes/VulkanSDK/1.2.198.1/macOS/bin/glslc shader.vert -o ../vert.spv
+    /Users/Lewis/Codes/VulkanSDK/1.2.198.1/macOS/bin/glslc shader.frag -o ../frag.spv
 fi
